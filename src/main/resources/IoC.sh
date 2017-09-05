09bbc1470afd7833908f61d74d76ef162e5be39c
java <java@bigdata>
2017-09-05 12:15:16 +0900

    IoC study

---
+++ src/main/resources/IoC.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env scala
+
+println("##############################");
+println("  IoC (Inversion of Control)  ");
+println("          제어의 역전         ");
+println(" 1.Interface                  ");
+println(" 2.Abstract Class             ");
+println(" 3.Annotation                 ");
+println(" 4.POJO(Plain Old Java Object)");
+println("##############################");
